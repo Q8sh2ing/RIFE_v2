1b1db81fb5ef41bed9ee122d0dee77b045762455
Q8sh2ing <121156896+Q8sh2ing@users.noreply.github.com>
2023-03-27 02:52:23 +0800

    Update inference_video.py

--- inference_video.py
+++ inference_video.py
@@ -242,7 +242,7 @@ while True:
         ssim = ssim_matlab(I0_small[:, :3], I1_small[:, :3])
         frame = (I1[0] * 255).byte().cpu().numpy().transpose(1, 2, 0)[:h, :w]
     
-    if ssim < 0.5:
+    if ssim < 1.0:
         output = []
         for i in range((2 ** args.exp) - 1):
             output.append(I0)
